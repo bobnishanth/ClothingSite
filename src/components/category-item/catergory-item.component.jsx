8326de12f640b1e529b713e8f3d143f614f17b58
Nish <nish@Nishanths-MacBook-Air.local>
2022-06-30 16:08:27 -0400

    final changes

--- src/components/category-item/catergory-item.component.jsx
+++ src/components/category-item/catergory-item.component.jsx
@@ -1,19 +1,19 @@
 import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import './category-item.styles.scss'
 const CategoryItem = (props) => {
-  const { imageUrl, title } = props.category
-
+  const { imageUrl, title, route } = props.category
+  const navigate = useNavigate()
+  const navigationHandler = () => navigate(route)
   return (
-    <div className='category-container'>
+    <div className='category-container' onClick={navigationHandler}>
       <div
         className='background-image'
         style={{ backgroundImage: `url(${imageUrl})` }}
       />
       <div className='category-body-container'>
-        <Link to={`/shop/${title}`}>
-          <h2>{title.toUpperCase()}</h2>
-          <p>Shop Now</p>
-        </Link>
+        <h2>{title.toUpperCase()}</h2>
+        <p>Shop Now</p>
       </div>
     </div>
   )
